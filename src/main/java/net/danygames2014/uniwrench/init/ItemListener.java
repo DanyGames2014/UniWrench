bf588f72843fdf5149eb0f62bf851d08bad4b8b6
Daniel Fildán <fildand@centrum.cz>
2023-12-25 22:07:54 +0100

    Initial Commit

---
+++ src/main/java/net/danygames2014/uniwrench/init/ItemListener.java
@@ -0,0 +1,21 @@
+package net.danygames2014.uniwrench.init;
+
+import net.danygames2014.uniwrench.item.UniversalWrench;
+import net.mine_diver.unsafeevents.listener.EventListener;
+import net.minecraft.item.Item;
+import net.modificationstation.stationapi.api.event.registry.ItemRegistryEvent;
+import net.modificationstation.stationapi.api.mod.entrypoint.Entrypoint;
+import net.modificationstation.stationapi.api.util.Namespace;
+import net.modificationstation.stationapi.api.util.Null;
+
+public class ItemListener {
+    @Entrypoint.Namespace
+    public static final Namespace MOD_ID = Null.get();
+
+    public static Item universalWrench;
+
+    @EventListener
+    public void registerWrench(ItemRegistryEvent event){
+        universalWrench = new UniversalWrench(MOD_ID.id("universal_wrench")).setTranslationKey(MOD_ID,"universal_wrench");
+    }
+}
---
+++ src/main/java/net/danygames2014/uniwrench/item/UniversalWrench.java
@@ -0,0 +1,10 @@
+package net.danygames2014.uniwrench.item;
+
+import net.modificationstation.stationapi.api.template.item.TemplateItem;
+import net.modificationstation.stationapi.api.util.Identifier;
+
+public class UniversalWrench extends TemplateItem {
+    public UniversalWrench(Identifier identifier) {
+        super(identifier);
+    }
+}
