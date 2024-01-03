4155d40b77629a9f9b51ff5312a6f8732f28414e
Daniel Fildán <fildand@centrum.cz>
2024-01-03 05:10:24 +0100

    A single line of documentation

--- src/main/java/net/danygames2014/uniwrench/api/Wrenchable.java
+++ src/main/java/net/danygames2014/uniwrench/api/Wrenchable.java
@@ -4,6 +4,9 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 
+/**
+ * An interface to define a block which wrenches can interact with
+ */
 public interface Wrenchable {
     default void wrenchRightClick(ItemStack stack, PlayerEntity player, boolean isSneaking, World world, int x, int y, int z, int side, WrenchMode wrenchMode) {
     }
